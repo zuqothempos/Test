ab2fc1b04f2837628c18871c5aa5d996795bf6e8
zuqothempos <alexandretheoo@icloud.com>
2026-02-22 20:58:02 +0100

    Add files via upload

---
+++ KeySystem.lua
@@ -0,0 +1,25 @@
+ocal KeySystem = loadstring(game:HttpGet("https://raw.githubusercontent.com/OopssSorry/LuaU-Free-Key-System-UI/main/source.lua"))()
+local KeyValid = false
+local response = KeySystem:Init({
+	Debug=false, -- <bool> Prints some output in console when true
+	Title="MNCStorm Bêta", -- <string or nil> Title of key system
+	Description=nil, -- <string or nil> Description of key system
+	Link="", -- <string> String to get key
+	Discord="https://discord.gg/wS5JgH3dV6", -- <string or nil> Button to join your discord server
+	SaveKey=false, -- <bool or nil> Just auto save key
+	Verify=function(key) -- <function> Verify is key valid
+		if key=="Admin!" then
+      KeyValid=true
+			return true
+	      
+		else
+			return false
+		end
+	end,
+	GuiParent = game.CoreGui, -- <object or nil> :3
+})
+
+if not response or not KeyValid then return end
+if KeyValid then
+  local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/zuqorl/CDTStorm/refs/heads/main/template.lua"))()
+end
